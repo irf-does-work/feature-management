8ee5b7d0d48ae8615a0451c99033b6ff5ad35ca3
irf-does-work <irfan.shakir@geekywolf.com>
2024-11-12 18:13:51 +0530

    added folders

---
+++ FeatureToggle.Domain/Entity/Custom Schema/Business.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeatureToggle.Domain.Entity.Custom_Schema
+{
+    public class Business
+    {
+    
+    }
+}
