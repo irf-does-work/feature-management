9c3c6422ca1d8cda2f7189fc6880031b368d33a5
irf-does-work <irfan.shakir@geekywolf.com>
2024-11-15 19:29:25 +0530

    renaming usercontext

--- FeatureToggle.Infrastructure/Models/UserContext.cs
+++ FeatureToggle.Infrastructure/Models/UserContext.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FeatureToggle.Domain.Configurations;
-using FeatureToggle.Domain.Entity.FeatureManagementSchema;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using FeatureToggle.Domain.Configurations;
+//using FeatureToggle.Domain.Entity.FeatureManagementSchema;
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
 
 
-namespace FeatureToggle.Infrastructure.Models
-{
-    public class UserContext : IdentityDbContext<User>
-    {
-        public DbSet<Log> Logs { get; set; }  //Log to logs
-        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+//namespace FeatureToggle.Infrastructure.Models
+//{
+//    public class UserContext : IdentityDbContext<User>
+//    {
+//        public DbSet<Log> Logs { get; set; }  //Log to logs
+//        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder); // for Identity
-            modelBuilder.ApplyConfiguration(new LogConfiguration());
-            modelBuilder.ApplyConfiguration(new UserConfiguration());
-            //modelBuilder.AddConventions
-        }
+//        protected override void OnModelCreating(ModelBuilder modelBuilder)
+//        {
+//            base.OnModelCreating(modelBuilder); // for Identity
+//            modelBuilder.ApplyConfiguration(new LogConfiguration());
+//            modelBuilder.ApplyConfiguration(new UserConfiguration());
+//            //modelBuilder.AddConventions
+//        }
 
-    }
-}
+//    }
+//}
