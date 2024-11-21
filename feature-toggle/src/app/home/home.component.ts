c3588c893815cdab9eb24a701cafe4fcfd2aaee3
irf-does-work <irfan.shakir@geekywolf.com>
2024-11-21 12:50:20 +0530

    filter dropdown changes

--- feature-toggle/src/app/home/home.component.ts
+++ feature-toggle/src/app/home/home.component.ts
@@ -2,18 +2,20 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FeatureCardComponent } from '../feature-card/feature-card.component';
 import { FeatureStatus, FeatureType } from '../enum/feature.enum';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FeatureCardComponent],
+  imports: [NavbarComponent, FeatureCardComponent,FormsModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   featureTypeEnum = FeatureType;  
   featureStatusEnum = FeatureStatus; 
-
+  rtCheckboxSate: boolean = false
 
   selectedFilters: string[] = [];
 
