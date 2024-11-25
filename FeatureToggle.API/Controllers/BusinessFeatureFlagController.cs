7b76bef8c533cc42d27538796368604b6aaa4e6b
cyrilkurian2 <cyril.kurian@geekywolf.com>
2024-11-25 16:13:51 +0530

    backend clean up

--- FeatureToggle.API/Controllers/BusinessFeatureFlagController.cs
+++ FeatureToggle.API/Controllers/BusinessFeatureFlagController.cs
@@ -9,30 +9,6 @@ namespace FeatureToggle.API.Controllers
     [ApiController]
     public class BusinessFeatureFlagController(IMediator mediator) : ControllerBase
     {
-        [HttpPost("release/enable")]
-        public async Task<int> EnableRelease(EnableReleaseCommand command)
-        {
-           return await mediator.Send(command);
-        }
-
-        [HttpPost("release/disable")]
-        public async Task<int> DisableRelease(DisableReleaseCommand command)
-        {
-            return await mediator.Send(command);
-        }
-
-        [HttpPost("feature/enable")]
-        public async Task<int> EnableFeature(EnableFeatureCommand command)
-        {
-            return await mediator.Send(command);
-        }
-
-        [HttpPost("feature/disable")]
-        public async Task<int> DisableFeature(DisableFeatureCommand command) 
-        {
-            return await mediator.Send(command);
-        }
-
         [HttpPost("feature/update")]
         public async Task<int> UpdateFeature(UpdateToggleCommand command) 
         {
