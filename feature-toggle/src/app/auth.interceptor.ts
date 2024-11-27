9fd1f825fc3d3e9f9942b4897df2af104f40ce81
cyrilkurian2 <cyril.kurian@geekywolf.com>
2024-11-27 12:40:57 +0530

    added bearer token in header

---
+++ feature-toggle/src/app/auth.interceptor.ts
@@ -0,0 +1,18 @@
+import {HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { TOKEN_KEY } from './shared/constants';
+
+export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) => {
+  const authToken = localStorage.getItem(TOKEN_KEY);
+
+  // console.log("console from auth interceptor\n" + `Bearer ${authToken}`);
+
+  if (!authToken) {
+    return next(req);
+  }
+
+  const newRq = req.clone({
+    headers: req.headers.set('Authorization', `Bearer ${authToken}`),
+  });
+
+  return next(newRq);
+};
